2af78b42fdf6a13327ad7167d4bebaf8566dfd7f
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2024-05-07 18:26:42 +0700

    update

--- app/Imports/SiswaImport.php
+++ app/Imports/SiswaImport.php
@@ -2,11 +2,13 @@
 
 namespace App\Imports;
 
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithStartRow;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class SiswaImport implements ToModel, WithStartRow
 {
@@ -17,6 +19,10 @@ class SiswaImport implements ToModel, WithStartRow
 
     public function model(array $row)
     {
+        // Konversi format tanggal dari "30/05/2024" menjadi "2024-05-30"
+        // dd($row[6]);
+        // $tgl_lahir = Carbon::createFromFormat('d/m/Y', $row[6])->format('Y-m-d');
+
         return new User([
             'name' => $row[0],
             'email' => $row[1],
@@ -24,10 +30,11 @@ class SiswaImport implements ToModel, WithStartRow
             'alamat' => $row[3],
             'no_hp' => $row[4],
             'tempat_lahir' => $row[5],
-            'tgl_lahir' => $row[6],
+            'tgl_lahir' => Date::excelToDateTimeObject($row[6])->format('Y-m-d'), // Tanggal lahir dalam format "YYYY-MM-DD"
             'jk' => $row[7],
             'kelas' => $row[8],
             'password' => '12345678',
+            'role' => 'siswa',
         ]);
     }
 }
