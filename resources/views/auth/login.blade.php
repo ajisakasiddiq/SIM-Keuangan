1239ec5766388aa5691a23398fd15578f0cf47cf
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2024-05-16 01:51:45 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,40 @@
 @extends('layouts.auth')
-@section('title')
 
+@section('title')
+    Sign In
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card border-0 shadow rounded-3 my-5">
-          <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-              <div class="form-floating mb-3">
-                  <label for="floatingInput">Email address</label>
-                <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
-              </div>
-              <div class="form-floating mb-3">
-                  <label for="floatingPassword">Password</label>
-                <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
-              </div>
-
-              <div class="form-check mb-3">
-                <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
-                <label class="form-check-label" for="rememberPasswordCheck">
-                  Remember password
-                </label>
-              </div>
-              <div class="d-grid">
-                <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Sign
-                  in</button>
-              </div>
-            </form>
-          </div>
+<div class="container-fluid" style="background-image: url('/img/image.png'); background-size: cover; height: 100vh;">
+    <div class="row justify-content-center align-items-center" style="height: 100%;">
+        <div class="col-sm-9 col-md-7 col-lg-5">
+            <div class="card border-0 shadow rounded-3 my-5">
+              <div class="card-body p-4 p-sm-5">
+                {{-- <h5 class="card-title text-center mb-4 fw-light fs-5">Sign In</h5> --}}
+                <h2 class="text-center mb-2">SIM-K</h2>
+                <h2 class="text-center mb-4">MTs. Zainul Hasan Balung</h2>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="rememberPasswordCheck">
+                        <label class="form-check-label" for="rememberPasswordCheck">Remember password</label>
+                    </div>
+                    <div class="d-grid">
+                        <button type="submit" class="btn btn-primary btn-login text-uppercase fw-bold">Sign in</button>
+                    </div>
+                </form>
+            </div>
+            
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 @endsection
-@push('addon-script')
-@endpush
